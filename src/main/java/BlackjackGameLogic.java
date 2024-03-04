f8afd55ff57e35a16b0b6bc86edf8283cb5ec414
kyle <kylegle9842@gmail.com>
2024-03-03 21:23:15 -0600

    Not a requirement but implementing split and insurance are kinda crucial to the gameplay of blackjack. Also implementing a card counting helper would make this actually useful (also not a requirement).

--- src/main/java/BlackjackGameLogic.java
+++ src/main/java/BlackjackGameLogic.java
@@ -1,2 +1,54 @@
+/*
+* Author: Kyle Gleason
+* TODO: If time, implement splitting and insurance (not a requirement but is crucial to blackjack)
+* */
+import java.util.ArrayList;
+
 public class BlackjackGameLogic {
+
+    /*
+    * Counts the value of all the cards in a hand, 2-10 (keep value), face cards (10) ace (1 or 11)
+    * Currently has no logic pertaining if an ace should be a 1 or an 11 but will possibly be updated
+    * */
+    public int handTotal(ArrayList<Card> hand) {
+        int size = 0;
+        for (Card card : hand) {
+            size += card.value;
+        }
+        return size;
+    }
+
+    /*
+    * Most of the logic for determining if drawing a card results in a win/lose for dealer or player will be in
+    * BlackjackGame.java. Here we are just assuming that this function is called when either
+    *       A.) The player or dealer hit 21 (not both)
+    *       B.) The player or dealer both choose to stand
+    *       C.) The player or the dealer hands sum up to over 21
+    * While there are many more niche ways to win, loose, or push (not really just not typing out every possible)
+    * The gist is this function should only be called once per hand as you can only win a hand once
+    * */
+    public String whoWon(ArrayList <Card> playerHand1, ArrayList<Card> dealerHand) {
+        int playerHand1Total = handTotal(playerHand1);
+        int dealerHandTotal = handTotal(dealerHand);
+
+        if ((playerHand1Total > dealerHandTotal) && (playerHand1Total < 22)) {
+            return "player";
+        } else if ((dealerHandTotal > playerHand1Total) && (dealerHandTotal < 22)) {
+            return "dealer";
+        }
+        else if (dealerHandTotal == playerHand1Total) {
+            return "push";
+        }
+        // *** REMOVE FOR TESTING ***
+        else {
+            return "ERROR IN LOGIC";
+        }
+    }
+
+    /*
+    * Returns true if the dealer must draw another card (dealer must hit on 16 or less)
+    * */
+    public boolean evaluateBankerDraw (ArrayList<Card> hand) {
+        return handTotal(hand) <= 16;
+    }
 }
--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,2 +1,15 @@
+/*
+* Author: Kyle Gleason
+* This class represents the playing card with two variables (blackjack only cares about value) string suit, int value
+* the suit holds the suit and the value holds the value of the card (where 2-10 is 2-10, face cards are 10, aces will
+* initially be 11 but can change to 1 if adding 11 goes over 21) <--- current logic is subject to possible change.
+* */
+
 public class Card {
+    String suit;
+    int value;
+    Card(String theSuit, int theValue) {
+        suit = theSuit;
+        value = theValue;
+    }
 }
