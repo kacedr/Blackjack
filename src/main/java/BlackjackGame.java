dc1eb28bb97afaa9fd3b3c7b0e0b800c49e22e23
kyle <kylegle9842@gmail.com>
2024-03-03 21:33:08 -0600

    More setting up of class files, front end can start being developed.

--- src/main/java/BlackjackGame.java
+++ src/main/java/BlackjackGame.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class BlackjackGame {
+    ArrayList<Card> playerHand, bankerHand;
+
+    BlackjackDealer theDealer;
+    BlackjackGameLogic gameLogic;
+    double currentBet;
+    double currentWinnings;
+    public double evaluateWinnings() {
+        return 0.0;
+    }
 }
